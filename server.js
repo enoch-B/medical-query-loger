b72af49740b43f62d59580be7db2d04aa2d04c40
enoch B <henokt828@gmail.com>
2026-01-21 16:53:45 +0300

    feat: Initialize server listening on a configurable port, add Docker environment variables, and include a `nodemon` dev script.

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-import app from "./app";
+import app from "./app.js";
 import dotenv from "dotenv"
 
 dotenv.config()
+
+const PORT = process.env.PORT || 3000
+if (!PORT) {
+    console.warn("The Port from .env is not Used")
+}
+
+app.listen(
+    PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    }
+)
