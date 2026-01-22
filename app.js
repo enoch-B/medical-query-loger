5f673856be440e4da339aaa398351d83171d8f0c
enoch B <henokt828@gmail.com>
2026-01-22 22:54:09 +0300

    feat: Implement medical query logging API with dedicated routes, controller, Prisma setup, and Docker-compose volume additions.

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 import express from "express"
+import queriesRoute from "./routes/queriesRoute.js";
 
-const app= express()
+const app = express()
 
 app.use(express.json());
 
-app.get("/health", (req, res) =>{
-res.json({status:"ok", service:"Media Query logger"});
+app.get("/health", (req, res) => {
+    res.json({ status: "ok", service: "Media Query logger" });
+});
+
+app.use("/api", queriesRoute);
+
+//Endpoint logger
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+//Error handler
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
 });
 
 export default app;
