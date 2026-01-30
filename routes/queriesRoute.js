a11edf5ab862e90758cd7117cc9faca28f45b1df
enoch B <henokt828@gmail.com>
2026-01-30 21:00:45 +0300

    feat: Add authentication middleware to queries route for secure access to create and retrieve queries endpoints

--- routes/queriesRoute.js
+++ routes/queriesRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createQuery, getAllQueries } from "../controllers/queryController.js";
+import { authenticate } from "../middleware/authenticate.js";
 
 const router = express.Router();
 
-router.post("/create", createQuery);
-router.get("/get", getAllQueries);
+router.post("/create", authenticate, createQuery);
+router.get("/get", authenticate, getAllQueries);
 
 export default router;
