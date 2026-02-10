4a127ddaf6a57c6ad8da123fc29e688d126b6dee
enoch B <henokt828@gmail.com>
2026-02-10 23:03:15 +0300

    feat: Add new API routes for creating, retrieving, uploading, and getting queries.

--- routes/queriesRoute.js
+++ routes/queriesRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { createQuery, getAllQueries } from "../controllers/queryController.js";
 import { authenticate } from "../middleware/authenticate.js";
+import { uploadFile, getFile } from "../controllers/queryController.js";
 
 const router = express.Router();
 
 router.post("/create", authenticate, createQuery);
 router.get("/get", authenticate, getAllQueries);
+router.post("/upload", authenticate, uploadFile);
+router.get("/file/:id", authenticate, getFile);
 
 export default router;
